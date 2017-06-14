6f2a17c37e837bfc9d4ee9499d5b993e7e201e4b
sircortly <lyons.cort@gmail.com>
2017-06-14 19:04:35 -0400

    Initialize basic Slim application

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
 
-phpinfo();
+// Require Composer Autoload
+require './vendor/autoload.php';
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$config = [
+    'settings' => [
+        'displayErrorDetails' => true
+    ]
+];
+
+// Instantiate Slim App
+$app = new \Slim\App($config);
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('Slim Application');
+    return $response;
+});
+$app->run();
 
