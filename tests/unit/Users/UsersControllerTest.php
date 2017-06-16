e0666cc18ec39398f95eebcfe0ba077713773369
sircortly <lyons.cort@gmail.com>
2017-06-16 15:40:36 -0400

    Create skeleton for UsersController unit tests

---
+++ tests/unit/Users/UsersControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Users\UsersController;
+
+class UsersControllerTest extends TestCase
+{
+    public function testIndex()
+    {
+        $this->assertTrue(true);
+    }
+}
