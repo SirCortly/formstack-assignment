507613afa9f0cd8e763953576373e38d283c74de
sircortly <lyons.cort@gmail.com>
2017-06-18 12:21:53 -0400

    Add transformCollection method to AbstractViewTransformer

--- src/App/AbstractViewTransformer.php
+++ src/App/AbstractViewTransformer.php
@@ -6,6 +6,18 @@ use App\AbstractDomainObject;
 abstract class AbstractViewTransformer
 {
     /**
+     * Transform a collection of entities to output array
+     *
+     * @param AbstractDomainObject[] $entities Entities to transform
+     */
+    public function transformCollection(array $entities)
+    {
+        return array_map(function($entity) {
+            return $this->transform($entity);
+        }, $entities);
+    }
+
+    /**
      * Transform an entity to output array
      *
      * @param AbstractDomainObject $entity Entity to transform
---
+++ tests/unit/AbstractViewTransformerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\{AbstractDomainObject, AbstractViewTransformer};
+
+class AbstractViewTransformerTest extends TestCase
+{
+/**
+    * Test that collection is transformed to array
+    */
+public function testTransformCollection()
+{
+    $transformer = $this->getMockForAbstractClass(AbstractViewTransformer::class);
+
+    // Set up a collection with two domain objects
+    $collection = [
+        $this->getMockForAbstractClass(AbstractDomainObject::class),
+        $this->getMockForAbstractClass(AbstractDomainObject::class)
+    ];
+
+    // Expect transform to be called twice, once on each domain object
+    $transformer->expects($this->exactly(2))
+        ->method('transform')
+        ->will($this->returnValue(['transformed' => 'values']));
+
+    $result = $transformer->transformCollection($collection);
+
+    // We should end up with a multidimensional array
+    // with one set of values for each object in collection
+        $this->assertEquals([
+            [
+                'transformed' => 'values'
+            ],
+            [
+                'transformed' => 'values'
+            ]
+        ], $result);
+    }
+}
