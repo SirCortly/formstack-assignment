861cbc43b8e5d48e6e5840429e2e5dbf3e56cf43
sircortly <lyons.cort@gmail.com>
2017-06-18 13:47:55 -0400

    Update functional test for GET /users

--- tests/functional/UsersTest.php
+++ tests/functional/UsersTest.php
@@ -5,59 +5,135 @@ use GuzzleHttp\Client;
 
 class UsersTest extends TestCase
 {
+    /**
+     * @var GuzzleHttp\Cleint HTTP Client
+     */
     protected $http;
+
+    /**
+    * @var PDO Databse connection
+    */
+    protected $pdo;
+
+    /**
+    * @var array Test Fixtures
+    */
+    private $fixtures = [
+        [
+            'email' => 'john.lennon@beatles.com',
+            'password' => 'john',
+            'firstname' => 'John',
+            'lastname' => 'Lennon'
+        ],
+        [
+            'email' => 'george.harrison@beatles.com',
+            'password' => 'george',
+            'firstname' => 'George',
+            'lastname' => 'Harrison'
+        ],
+        [
+            'email' => 'paul.mcartney@beatles.com',
+            'password' => 'paul',
+            'firstname' => 'Paul',
+            'lastname' => 'Mcartney'
+        ],
+        [
+            'email' => 'ringo.starr@beatles.com',
+            'password' => 'ringo',
+            'firstname' => 'Ringo',
+            'lastname' => 'Starr'
+        ]
+    ];
 
     protected function setUp()
     {
         $this->http = new Client([
             'base_uri' => 'http://www.testbox.dev'
         ]);
+
+        $this->pdo = new PDO('mysql:host=localhost;dbname=my_app', 'my_app', 'secret');
+
+        // Truncate table
+        $this->pdo->exec("TRUNCATE TABLE `users`;");
+
+        // Load fixtures into DB
+        foreach ($this->fixtures as $fixture) {
+            $this->pdo->prepare("
+                INSERT INTO `users` (
+                    `email`, `password`, `firstname`, `lastname`, `created_at`
+                )
+                VALUES
+                    (?, ?, ?, ?, '2017-06-18 00:00:00');
+            ")->execute([
+                $fixture['email'],
+                $fixture['password'],
+                $fixture['firstname'],
+                $fixture['lastname']
+            ]);
+        }
     }
 
     protected function tearDown()
     {
         $this->http = null;
+
+        $this->pdo->exec("TRUNCATE TABLE `users`;");
+        $this->pdo = null;
     }
 
     public function testGetUsers()
     {
         // GET /users
         $response = $this->http->get('/users');
-        $data = json_decode($response->getBody(), true);
-
-        $data_expectation = [
-            'data' => [
-                [
-                    'email' => 'john.lennon@beatles.com',
-                    'firstname' => 'John',
-                    'lastname' => 'Lennon',
-                ],
-                [
-                    'email' => 'george.harrison@beatles.com',
-                    'firstname' => 'George',
-                    'lastname' => 'Harrison',
-                ],
-                [
-                    'email' => 'paul.mcartney@beatles.com',
-                    'firstname' => 'Paul',
-                    'lastname' => 'Mcartney',
-                ],
-                [
-                    'email' => 'ringo.starr@beatles.com',
-                    'firstname' => 'Ringo',
-                    'lastname' => 'Starr',
-                ]
-            ]
-        ];
 
         // Assert response 200 OK
         $this->assertEquals(200, $response->getStatusCode());
 
         // Assert Content-Type applciation/json
         $this->assertEquals(
-            'application/json',
+            'application/json;charset=utf-8',
             $response->getHeaders()['Content-Type'][0]
         );
+
+        $data = json_decode($response->getBody(), true);
+        $data_expectation = [
+            [
+                'id' => 1,
+                'email' => 'john.lennon@beatles.com',
+                'firstname' => 'John',
+                'lastname' => 'Lennon',
+                'fullname' => 'John Lennon',
+                'created_at' => '2017-06-18 00:00:00',
+                'updated_at' => null
+            ],
+            [
+                'id' => 2,
+                'email' => 'george.harrison@beatles.com',
+                'firstname' => 'George',
+                'lastname' => 'Harrison',
+                'fullname' => 'George Harrison',
+                'created_at' => '2017-06-18 00:00:00',
+                'updated_at' => null
+            ],
+            [
+                'id' => 3,
+                'email' => 'paul.mcartney@beatles.com',
+                'firstname' => 'Paul',
+                'lastname' => 'Mcartney',
+                'fullname' => 'Paul Mcartney',
+                'created_at' => '2017-06-18 00:00:00',
+                'updated_at' => null
+            ],
+            [
+                'id' => 4,
+                'email' => 'ringo.starr@beatles.com',
+                'firstname' => 'Ringo',
+                'lastname' => 'Starr',
+                'fullname' => 'Ringo Starr',
+                'created_at' => '2017-06-18 00:00:00',
+                'updated_at' => null
+            ]
+        ];
 
         $this->assertEquals($data_expectation, $data);
     }
