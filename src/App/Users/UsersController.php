14246003a6b0c153c62664dc9f514ef1a3a5a351
sircortly <lyons.cort@gmail.com>
2017-06-18 15:00:05 -0400

    Implement UsersController create method

--- src/App/Users/UsersController.php
+++ src/App/Users/UsersController.php
@@ -59,4 +59,44 @@ class UsersController extends AbstractController
             200
         );
     }
+
+    /**
+     *  POST /users
+     *
+     * Create new User
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function create(Request $request, Response $response)
+    {
+        $data = $request->getParsedBody();
+        $mapper = new UserDataMapper($this->container->get('db'));
+
+        // Create and populate new User
+        $user = new User();
+        isset($data['email']) ? $user->setEmail($data['email']) : '';
+        isset($data['password']) ? $user->setPassword($data['password']) : '';
+        isset($data['firstname']) ? $user->setFirstname($data['firstname']) : '';
+        isset($data['lastname']) ? $user->setLastname($data['lastname']) : '';
+
+        $validation_errors = $user->validate();
+        if ($validation_errors) {
+            return $response->withJson(
+                $validation_errors,
+                422
+            );
+        }
+
+        $id = $mapper->save($user);
+
+        $transformer = new UserViewTransformer();
+
+        return $response->withJson(
+            $transformer->transform($mapper->fetchById($id)),
+            201
+        );
+    }
 }
--- src/routes.php
+++ src/routes.php
@@ -4,4 +4,5 @@
  */
 
 $app->get('/users', UsersController::class . ':index');
+$app->post('/users', UsersController::class . ':create');
 $app->get('/users/{id}', UsersController::class . ':show');
