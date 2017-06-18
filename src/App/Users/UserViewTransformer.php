2aaf87bc607ad6cfeabfaedd7d9e844daf4a1508
sircortly <lyons.cort@gmail.com>
2017-06-18 13:47:11 -0400

    Add id and update update_at in UserViewTransformer

--- src/App/Users/UserViewTransformer.php
+++ src/App/Users/UserViewTransformer.php
@@ -13,12 +13,15 @@ class UserViewTransformer extends AbstractViewTransformer
     public function transform(AbstractDomainObject $user) : array
     {
         return [
+            'id' => $user->getId(),
             'email' => $user->getEmail(),
             'firstname' => $user->getFirstname(),
             'lastname' => $user->getLastname(),
             'fullname' => $user->getFullName(),
             'created_at' => $user->getCreatedAt()->format('Y-m-d H:i:s'),
-            'updated_at' => $user->getUpdatedAt()->format('Y-m-d H:i:s')
+            'updated_at' => is_null($user->getUpdatedAt())
+                ? null
+                : $user->getUpdatedAt()->format('Y-m-d H:i:s')
         ];
     }
 }
--- tests/unit/Users/UserViewTransformerTest.php
+++ tests/unit/Users/UserViewTransformerTest.php
@@ -11,19 +11,24 @@ class UserViewTransformerTest extends TestCase
     public function testTransform()
     {
         $user = new User();
+        $user->setId(101);
         $user->setEmail('brendon@fraiser.com');
         $user->setPassword('immotep');
         $user->setFirstname('Brendon');
         $user->setLastname('Fraiser');
+        $user->setCreatedAt(new \DateTime('2017-06-18 00:00:00'));
 
         $transformer = new UserViewTransformer();
         $response = $transformer->transform($user);
 
         $this->assertEquals([
+            'id' => 101,
             'email' => 'brendon@fraiser.com',
             'firstname' => 'Brendon',
             'lastname' => 'Fraiser',
             'fullname' => 'Brendon Fraiser',
+            'created_at' => '2017-06-18 00:00:00',
+            'updated_at' => null,
         ], $response);
     }
 }
