c87716640110b43816b7cf879fc8b76a42c38466
sircortly <lyons.cort@gmail.com>
2017-06-18 12:44:45 -0400

    Implement UsersController index() method

--- src/App/Users/UsersController.php
+++ src/App/Users/UsersController.php
@@ -3,17 +3,30 @@ namespace App\Users;
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use App\AbstractController;
 
-class UsersController
+class UsersController extends AbstractController
 {
-    public function __construct()
+    /**
+     * Index method for GET /users
+     *
+     * Return a list of Users
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function index(Request $request, Response $response)
     {
-    }
+        $mapper = new UserDataMapper($this->container->get('db'));
+        $users = $mapper->fetchAll();
 
-    public function index(Request $request, Response $response, $args)
-    {
-        return $response->withJson([
-            'data' => 'test'
-        ]);
+        $transformer = new UserViewTransformer();
+
+        return $response->withJson(
+            $transformer->transformCollection($users),
+            200
+        );
     }
 }
